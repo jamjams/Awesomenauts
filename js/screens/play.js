fb7529b38680bd3f195c4f0c5958cfa0719a09df
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-01-22 10:54:59 -0800

    video 5 showing the map

--- js/screens/play.js
+++ js/screens/play.js
@@ -7,6 +7,7 @@ game.PlayScreen = me.ScreenObject.extend({
 		game.data.score = 0;
 
 		me.levelDirector.loadLevel("level01");
+		
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
