05e1d558965c0d60d3a3a9406100731b9706152e
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-01-22 11:09:47 -0800

    video 5 with comments

--- js/screens/play.js
+++ js/screens/play.js
@@ -5,9 +5,9 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
-
+/*showing the map*/
 		me.levelDirector.loadLevel("level01");
-		
+
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
