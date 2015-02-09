b2eb8e34059d37acebf16d9661079a5448c2b475
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-02-09 12:16:26 -0800

    video 18 code and comments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -206,3 +206,23 @@ game.EnemyBaseEntity = me.Entity.extend({
 	}
 });
 
+game.EnemyCreep = me.Entity.extend({
+	init: function(x, y, settings){
+		this._super(me.Entity, 'init', [x, y, {
+			image: "creep1",
+			width: 32,
+			height: 64,
+			spritewidth: "32",
+			spriteheight: "64",
+			getShape: function(){
+				return (new me.Rect(0, 0, 32, 64)).toPolygon();
+			}
+		}]);
+		this.health = 10;
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+
+	}
+});
