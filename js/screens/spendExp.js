d58d676f07caef2c41605ee5c21c10d4202b0ad8
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-03-12 11:10:29 -0700

    video 44 finishing SpendEXP screen

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -5,12 +5,20 @@ game.SpendExp = me.ScreenObject.extend({
 	onResetEvent: function() {
 	/*setting up the title screen*/	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+
+		me.input.bindKey(me.input.KEY.F1, "F1");
+		me.input.bindKey(me.input.KEY.F2, "F2");
+		me.input.bindKey(me.input.KEY.F3, "F3");
+		me.input.bindKey(me.input.KEY.F4, "F4");
+		me.input.bindKey(me.input.KEY.F5, "F5");
+
+
 		/*setting the key we need to press in order to start the game*/
 
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function() {
 				this._super(me.Renderable, 'init', [10, 10, 300, 50, me.game.viewport.width, me.game.viewport.height]);
-				this.font = new me.Font("Arial", 46, "purple");
+				this.font = new me.Font("Arial", 26, "purple");
 				/*listing for the pointer to be down*/
 				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 			},
@@ -19,13 +27,27 @@ game.SpendExp = me.ScreenObject.extend({
 				/*this where we set what is going to be printed out to the title screen and also setting the color of the text and what font the text is*/
 				this.font.draw(renderer.getContext(), "PRESS f1-f4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), "CURRENT EXP" + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), "F1: INCREMENT GOLD PRODUCTION " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
-				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 15);
-				this.font.draw(renderer.getContext(), "F3: INCREADE ATTACK DAMAGE " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 200);
-				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 250);
+				this.font.draw(renderer.getContext(), "F1: INCREMENT GOLD PRODUCTION CURRRENT LEVEL:" + game.data.exp1.toString() + " COST: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD ", this.pos.x, this.pos.y + 15);
+				this.font.draw(renderer.getContext(), "F3: INCREADE ATTACK DAMAGE ", this.pos.x, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH ", this.pos.x, this.pos.y + 250);
 				/*stylizing the Press enter to play text. tellng you how to start the game*/	
 			}
 		})));
+
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+			if(action === "F1"){
+
+			}else if(action === "F2"){
+
+			}else if(action === "F3"){
+
+			}else if(action === "F3"){
+
+			}else if(action === "F5"){
+				me.state.change(me.state.PLAY);
+			}
+		});
 
 	},
 	
@@ -34,5 +56,11 @@ game.SpendExp = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
+		me.input.bindKey(me.input.KEY.F1, "F1");
+		me.input.bindKey(me.input.KEY.F2, "F2");
+		me.input.bindKey(me.input.KEY.F3, "F3");
+		me.input.bindKey(me.input.KEY.F4, "F4");
+		me.input.bindKey(me.input.KEY.F5, "F5");
+		me.event.unsubscribe(this.handler);
 	}
 });
