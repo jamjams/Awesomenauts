d0026f8f30e8020a452b465a2728a2a8686fb8c2
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-03-27 10:22:52 -0700

    error with the enemy creep not appearing

---
+++ js/entities/gameTimerManager.js
@@ -0,0 +1,36 @@
+game.GameTimerManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date().getTime();
+		this.lastCreep = new Date().getTime();
+		this.paused = false;
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		this.now = new Date().getTime();
+		this.goldTimerCheck();
+
+		/*this line prevent s the above code from happening all the time*/
+	
+		
+		return true;
+	},
+
+	goldTimerCheck: function(){
+		if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
+			game.data.gold +=1;
+			/*console.log("Current gold" + game.data.gold);*/
+		}
+
+	},
+
+	creepTimerCheck: function(){
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			this.lastCreep = this.now;
+			var creep = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creep, 5);
+			console.log("Hello World");
+		}
+		/*return true;*/
+	}
+});
